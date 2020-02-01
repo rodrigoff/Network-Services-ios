e2986f356907d323764d521e1751883dc16313e3
rodrigoff@live.com <rodrigoff@live.com>
2020-02-01 08:49:01 -0300

    Initial Commit

---
+++ Network Services/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Network Services
+//
+//  Created by Rodrigo F. Fernandes on 01/02/20.
+//  Copyright © 2020 Rodrigo F. Fernandes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
